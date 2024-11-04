a9d5215f7f25a1f1c8aca9d8f43563ab21778463
hxt <hxt_4585@outlook.com>
2024-11-04 20:04:11 +0800

    java-code:3

--- springboot/springboot-003/src/main/java/com/hxt/Springboot003Application.java
+++ springboot/springboot-003/src/main/java/com/hxt/Springboot003Application.java
@@ -2,12 +2,14 @@ package com.hxt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Springboot003Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot003Application.class, args);
+        ConfigurableApplicationContext ioc = SpringApplication.run(Springboot003Application.class, args);
+        System.out.println(ioc.getBean("myConfig"));
     }
 
 }
